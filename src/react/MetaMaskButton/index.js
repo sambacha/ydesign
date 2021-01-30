4ad5f1b79f7083d2eca3e28e587211c329693f11
sam <sam@freighttrust.com>
2021-01-29 16:34:15 -0800

    yearn design system v0.1.0
    Signed-off-by: sam <sam@freighttrust.com>

---
+++ src/react/MetaMaskButton/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { ReactComponent as MMlogo } from './icon.svg';
+import Button, { StyledButton } from '../Button/BaseButton';
+
+const brandColors = {
+  baseColor: '#000',
+  hoverColor: '#4c4c4c',
+  activeColor: '#999',
+  foregroundColor: '#FFFFFF',
+};
+
+const MMStyledButton = styled(StyledButton)`
+  & {
+    transition: all 0.15s ease;
+  }
+
+  &:hover {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  && > svg.r-ff-icon {
+    height: ${props => (props.size === 'small' ? '1rem' : '1.5rem')};
+    width: ${props => (props.size === 'small' ? '1rem' : '1.5rem')};
+    margin-right: 0.5rem;
+    margin-bottom: -2px;
+  }
+`;
+
+const StyledButtonSolid = styled(MMStyledButton)`
+  & {
+    color: ${brandColors.foregroundColor};
+    background: ${brandColors.baseColor};
+  }
+  &:hover {
+    background-color: ${brandColors.hoverColor};
+  }
+  &:active {
+    background-color: ${brandColors.activeColor};
+  }
+`;
+
+const StyledButtonOutline = styled(MMStyledButton)`
+  & {
+    color: #333;
+    background: ${brandColors.foregroundColor};
+    border: 1px solid #ccc;
+  }
+  &:hover {
+    background-color: #f3f2f2;
+  }
+  &:active {
+    background-color: #e4e4e4;
+  }
+`;
+
+const ButtonBody = ({ children, size }) => (
+  <React.Fragment>
+    <MMlogo className={'r-ff-icon'} size={size} />
+    {children}
+  </React.Fragment>
+);
+
+const MetaMaskButtonSolid = ({ className, children, ...props }) => {
+  return (
+    <StyledButtonSolid className={className} {...props}>
+      <ButtonBody children={children} size={props.size} />
+    </StyledButtonSolid>
+  );
+};
+
+const MetaMaskButtonOutline = ({ className, children, ...props }) => {
+  return (
+    <StyledButtonOutline className={className} {...props}>
+      <ButtonBody children={children} size={props.size} />
+    </StyledButtonOutline>
+  );
+};
+
+const defaultProps = {
+  ...Button.defaultProps,
+  width: 'auto',
+  borderRadius: 1,
+  boxShadow: 1,
+};
+
+MetaMaskButtonSolid.defaultProps = defaultProps;
+MetaMaskButtonOutline.defaultProps = defaultProps;
+
+let MetaMaskButton;
+
+MetaMaskButton = MetaMaskButtonSolid;
+MetaMaskButton.Solid = MetaMaskButtonSolid;
+MetaMaskButton.Outline = MetaMaskButtonOutline;
+
+MetaMaskButton.propTypes = {
+  ...Button.propTypes,
+};
+
+MetaMaskButton.displayName = 'MetaMaskButton';
+
+export default MetaMaskButton;
---
+++ src/react/yearn-colors.js
@@ -0,0 +1,59 @@
+import { tint, shade, readableColor } from 'polished';
+
+const baseColors = {
+  black: '#000',
+  white: '#FFF',
+  blue: '#0657F9',
+  green: '#28C081',
+  yellow: '#FABF06',
+  red: '#EF1E02',
+  yearnblue: '#0657F9',
+  prussianblue: '#3259D6',
+};
+
+const colors = {
+  yearnblue: {
+    base: baseColors.yearnblue,
+    text: readableColor(baseColors.yearnblue),
+    light: [null, tint(0.2, baseColors.yearnblue)],
+    dark: [null, shade(0.2, baseColors.yearnblue)],
+  },
+  blue: {
+    base: baseColors.blue,
+    text: readableColor(baseColors.blue),
+    light: [null, tint(0.9, baseColors.blue)],
+    dark: [null, shade(0.4, baseColors.blue)],
+  },
+  green: {
+    base: baseColors.green,
+    text: baseColors.white,
+    light: [null, tint(0.9, baseColors.green)],
+    dark: [null, shade(0.4, baseColors.green)],
+  },
+  yellow: {
+    base: baseColors.yellow,
+    text: readableColor(baseColors.yellow),
+    light: [null, tint(0.9, baseColors.yellow)],
+    dark: [null, shade(0.4, baseColors.yellow)],
+  },
+  red: {
+    base: baseColors.red,
+    text: readableColor(baseColors.red),
+    light: [null, tint(0.9, baseColors.red)],
+    dark: [null, shade(0.4, baseColors.red)],
+  },
+};
+
+const yearnblue = colors.yearnblue;
+const blue = colors.blue;
+const green = colors.green;
+const yellow = colors.yellow;
+const red = colors.red;
+
+export { yearnblue };
+export { blue };
+export { green };
+export { yellow };
+export { red };
+
+export default baseColors;
